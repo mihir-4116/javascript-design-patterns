6b1bc96fe0d53673bff24ab0aa40f685643c97df
mihir-4116 <bhalalamark@gmail.com>
2022-12-26 23:10:06 +0530

    feat: examples updated

--- solid/dependency-inversion-principle.js
+++ solid/dependency-inversion-principle.js
@@ -7,34 +7,52 @@ It’s all about decoupling code.
 // Example :
 
 /*
-The issue with below example is it's completely depends on paypal api's.
+Let’s say we have a piece of software that runs an online store, and within that software one of the classes (PurchaseHandler) handles the final purchase. This class is capable of charging the user’s credit card, and does so by using a PayPal API:
 */
-class Checkout {
-  processPayment(paymentDetails, amount) {
-    const paymentSuccess = PayPal.makeRequest(paymentDetails, amount);
-    if (paymentSuccess) {
-      console.log("shopping successful");
+
+class PurchaseHandler {
+    processPayment(paymentDetails, amount) {
+        // Complicated, PayPal specific logic goes here
+        const paymentSuccess = PayPal.requestPayment(paymentDetails, amount);
+
+        if (paymentSuccess) {
+            // Do something
+            return true;
+        }
+
+        // Do something
+        return false;
     }
-    console.warn("oops! something went wrong");
-  }
 }
 
-// Solution //
-class Checkout {
-  processPayment(paymentDetails, amount) {
-    const paymentSuccess = PaymentHandler.requestPayment(
-      paymentDetails,
-      amount
-    );
-    if (paymentSuccess) {
-      console.log("shopping successful");
+/*
+The problem here is that if we change from PayPal to Square (another payment processor) in 6 months time, this code breaks. We need to go back and swap out our PayPal API calls for Square API calls. But in addition, what if the Square API wants different types of data? Or perhaps it wants us to “stage” a payment first, and then to process it once staging has completed?
+
+That’s bad, and so we need to abstract the functionality out instead.
+
+Rather than directly call the PayPal API from our payment page, we’ll instead create another class called PaymentHandler. The interface for this class will remain the same no matter what underlying payment system we use, even if the two systems are completely different. We’ll still need to make changes to the PaymentHandler interface if we change payment processor, but our higher level interface remains unchanged
+*/
+
+class PurchaseHandler {
+    processPayment(paymentDetails, amount) {
+        const paymentSuccess = PaymentHandler.requestPayment(
+            paymentDetails,
+            amount
+        );
+
+        if (paymentSuccess) {
+            // Do something
+            return true;
+        }
+
+        // Do something
+        return false;
     }
-    console.warn("oops! something went wrong");
-  }
 }
 
 class PaymentHandler {
-  makeRequest(paymentDetails, amount) {
-    return PayPal.requestPayment(paymentDetails, amount);
-  }
+    requestPayment(paymentDetails, amount) {
+        // Complicated, PayPal specific logic goes here
+        return PayPal.requestPayment(paymentDetails, amount);
+    }
 }
--- solid/interface-segregation-principle.js
+++ solid/interface-segregation-principle.js
@@ -6,17 +6,106 @@ The interface segregation principle states that an entity should never be forced
 that contains elements which it will never use
 */
 
-class People {}
+/*
+Imagine we have a streaming platform that has two types of users, a FreeUser and a PremiumUser, both extending from User
+*/
 
-class Bird {}
+// Example
+class User {
+  constructor(username) {
+    this.username = username;
+  }
 
-const flyer = {
-  fly: function () {
-    console.log("can fly");
+  skipAd() {
+    console.log(`I'm going to skip if I'm premium`);
+  }
+
+  startParty() {
+    console.log(`I'm going to start a party if I'm premium`);
+  }
+}
+
+class FreeUser extends User {
+  constructor(username) {
+    super(username);
+  }
+
+  skipAd() {
+    return null;
+  }
+
+  startParty() {
+    return null;
+  }
+}
+
+class PremiumUser extends User {
+  constructor(username) {
+    super(username);
+  }
+
+  skipAd() {
+    console.log(`Ad was skipped.`);
+  }
+
+  startParty() {
+    console.log("Party started, invite your friends!");
+  }
+}
+
+// The following code will execute
+// and print the message
+// const premium = new PremiumUser("premium_username");
+// premium.skipAd();
+// premium.startParty();
+
+// The following code will execute
+// but return null
+// const free = new FreeUser("free_username");
+// free.skipAd();
+// free.startParty();
+
+/*
+Taking into consideration that only a PremiumUser is able to skip ads and start parties, we are violating the Interface Segregation Prinicple by forcing both User and FreeUser to implement these actions. To apply the principle, we may use composition to only give these functionalities to PremiumUser. After applying the principle, the code would be the following
+*/
+
+class User {
+  constructor(username) {
+    this.username = username;
+  }
+}
+
+class FreeUser extends User {
+  constructor(username) {
+    super(username);
+  }
+}
+
+class PremiumUser extends User {
+  constructor(username) {
+    super(username);
+  }
+}
+
+const premiumBenefits = {
+  skipAd() {
+    console.log('Ad was skipped.');
+  },
+  startParty() {
+    console.log('Party started, invite your friends!');
   },
 };
 
-Object.assign(Bird.prototype, flyer);
+Object.assign(PremiumUser.prototype, premiumBenefits);
 
-const sparrow = new Bird();
-console.log(sparrow.fly());
+// The following code will execute
+// and print the message
+const premium = new PremiumUser('premium_username');
+premium.skipAd();
+premium.startParty();
+
+// The following code will throw an exception
+// because a FreeUser does not implement these methods
+const free = new FreeUser('free_username');
+free.skipAd();
+free.startParty();
